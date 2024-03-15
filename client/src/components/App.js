883d8952ddcd2ff7ba505239be40aab21c641288
Tanner Patterson <tpatterson5492@gmail.com>
2024-03-14 17:30:35 -0700

    created basic landing page setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Switch, Routes, Route } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage"
 
+import LandingPage from "./Landing";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>LandingPage</h1>}/>
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<LandingPage />}/>
+      <Route path="*" element={<NotFoundPage />}/>
     </Routes>
   )
 }
---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css'; // Import CSS file for styling
+
+const LandingPage = () => {
+  return (
+    <div className="background">
+      <div className="container">
+        <h1 className="title">Byte the Budget</h1>
+        <p className="subtitle">AI-Assisted Budgeting Made Easy</p>
+        <button className="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
