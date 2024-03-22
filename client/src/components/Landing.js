73bbbd8f6fd7c0e73aebbc2e31120692b8b463c6
Tanner Patterson <tpatterson5492@gmail.com>
2024-03-21 17:26:05 -0700

    updated different screen responsivness

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './LandingPage.css';
 
 const LandingPage = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(null); // State to track the index of the currently hovered item
-  const [gradientPosition, setGradientPosition] = useState({ x: 0, y: 0 }); // State to track the mouse cursor position for gradient effect
+  // State to track the index of the currently hovered item
+  const [hoveredIndex, setHoveredIndex] = useState(null); 
 
   // Event handler when mouse enters a list item
   const handleMouseEnter = (index) => {
@@ -15,25 +15,18 @@ const LandingPage = () => {
     setHoveredIndex(null); // Reset the hovered index when mouse leaves any list item
   };
 
-  // Event handler to track mouse movement for gradient effect
-  const handleMouseMove = (e) => {
-    const x = e.pageX - e.target.offsetLeft;
-    const y = e.pageY - e.target.offsetTop;
-    setGradientPosition({ x, y });
-  };
-
   // List of benefits
   const benefits = [
-    { title: 'Automated Expense Tracking', excerpt: 'Let our AI auto-matically track your expenses and categorize them for you.' },
-    { title: 'Smart Budget Thoughts', excerpt: 'Get customized budget tips based on your spending habits and financial goals.' },
-    { title: 'Personalized Financial Insights', excerpt: 'Gain valuable insights into your spending patterns and financial health with personalized reports.' },
-    { title: 'Expense Category Suggestions', excerpt: 'Get suggestions for optimizing your expense categories to better manage your budget.' },
-    { title: 'Real-time Spending Alerts', excerpt: 'Receive instant alerts and notifications to assist you in staying within your budget and avoiding over-spending.' },
+    { title: 'Automate Expense Tracking', excerpt: 'Let our AI auto-matically track your expenses and categorize them for you.' },
+    { title: 'Smart Budget Thoughts', excerpt: 'Get custom budget tips based on your spending habits and financial goals.' },
+    { title: 'Personal Financial Insights', excerpt: 'Gain valuable insights into your spending patterns and financial health with detailed personal reports.' },
+    { title: 'Expense Category Help', excerpt: 'Let our AI suggest options for optimizing your expense categories to better manage your budget.' },
+    { title: 'Real-time Spending Alerts', excerpt: 'Receive instant alerts to notify and assist you in staying within your budget and avoiding over-spending.' },
   ];
 
   return (
-    <div className="background" onMouseMove={handleMouseMove}>
-      <div className='mouse-capture' style={{ '--x': `${gradientPosition.x}px`, '--y': `${gradientPosition.y}px` }}>
+    <div className="background">
+      <div className='mouse-capture'>
         <div className="container">
           <h1 className="main-title">Byte the Budget</h1>
           <p className="subtitle">AI-Assisted Budgeting Made Easy</p>
@@ -47,8 +40,7 @@ const LandingPage = () => {
                 onMouseLeave={handleMouseLeave} 
               >
                 <div className='bullet-container'>
-                  <div className="ben-title">{hoveredIndex === index ? benefit.excerpt : benefit.title}</div> {/* Display excerpt if hovered over, otherwise display title */}
-                  <div className='ben-excerpt'></div>
+                  <div className={`ben-title animated-text ${hoveredIndex ? 'hovered' : ''}`}>{hoveredIndex === index ? benefit.excerpt : benefit.title}</div> {/* Display excerpt if hovered over, otherwise display title */}
                 </div>
               </li>
             ))}
