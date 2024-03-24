893b33d6650ebf7bf4d3adc799bff5b90cabc635
Takashi <th2826@columbia.edu>
2024-03-24 18:06:38 -0400

    feat: update sidebar styling

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -16,26 +16,26 @@ function SideBar() {
     <div className="side-bar">
       <div className="d-flex flex-column justify-content-between" style={{height: '100%'}}>
         <ul className="nav flex-column">
-          <NavLink to="/dashboard" end className={({ isActive }) => getNavLinkClass(isActive)}>
+          <NavLink to="/dashboard" end className={({ isActive }) => getNavLinkClass(isActive)} style={{ marginBottom: '12px' }}>
             <DashboardIcon style={{ marginRight: '8px' }}/>
             <span>Dashboard</span>
           </NavLink>
-          <NavLink to="/dashboard/plan" className={({ isActive }) => getNavLinkClass(isActive)}>
+          <NavLink to="/dashboard/plan" className={({ isActive }) => getNavLinkClass(isActive)} style={{ marginBottom: '12px' }}>
             <PlanIcon style={{ marginRight: '8px' }}/>
             <span>Plan</span>
           </NavLink>
-          <NavLink to="/dashboard/transactions" className={({ isActive }) => getNavLinkClass(isActive)}>
+          <NavLink to="/dashboard/transactions" className={({ isActive }) => getNavLinkClass(isActive)} style={{ marginBottom: '12px' }}>
             <TransactionsIcon style={{ marginRight: '8px' }}/>
             <span>Transactions</span>
           </NavLink>
         </ul>
 
         <ul className="nav flex-column">
-          <NavLink to="/dashboard/settings" className={({ isActive }) => getNavLinkClass(isActive)}>
+          <NavLink to="/dashboard/settings" className={({ isActive }) => getNavLinkClass(isActive)} style={{ marginBottom: '12px' }}>
             <SettingsIcon style={{ marginRight: '8px' }}/>
             <span>Settings</span>
           </NavLink>
-          <div>
+          <div style={{ marginBottom: '12px' }}>
             <LogoutIcon style={{ marginRight: '8px' }}/>
             <span>Logout</span>
           </div>
