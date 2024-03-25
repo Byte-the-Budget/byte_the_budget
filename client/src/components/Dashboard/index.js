e2f0510bfd010f500f6aaf5bb1017ea396cf75d4
Takashi <th2826@columbia.edu>
2024-03-24 21:02:16 -0400

    feat: update header

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -3,14 +3,23 @@ import { Link, Outlet } from 'react-router-dom'
 import SideBar from '../Sidebar';
 
 import './dashboard.css';
-
+import profileImage from '../../assets/profile.png'
+import logo from '../../assets/logo.png'
 function Dashboard() {
   return (
     <div className="content-wrapper">
       {/* Navbar Placeholder */}
       <nav className="nav-bar-placeholder d-flex align-items-center justify-content-between" style={{ width: '100%'}}>
-        <Link className="navbar-brand" to="/dashboard">LOGO here</Link>
-        <div>Right</div>
+        <Link className="navbar-brand" to="/dashboard">
+          <div className="logo-container">
+            <img src={logo}/>
+          </div>
+          <span>Byte the Budget</span>
+        </Link>
+        <div>
+          <img src={profileImage}/>
+          <span></span>
+        </div>
       </nav>
 
       <div className="layout">
